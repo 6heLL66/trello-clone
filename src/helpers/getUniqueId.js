7909ed4dd730838e3dd5203044ac7cc7abd72552
6heLL66 <51162582+6heLL66@users.noreply.github.com>
2021-04-21 11:52:28 +0300

    'fix'

--- src/helpers/getUniqueId.js
+++ src/helpers/getUniqueId.js
@@ -1,10 +1,9 @@
 export default function getUniqueId(type, size) {
-  let codes = [], id
+  let codes = []
   do {
     for (let i = 0; i < size; i++) {
       codes[i] = Math.round(Math.random() * (127 - 32) + 32)
     }
-    id = String.fromCharCode(...codes)
-  } while (type.find((e) => e.id === id))
-  return id
+  } while (type.find((e) => e.id === String.fromCharCode(...codes)))
+  return String.fromCharCode(...codes)
 }
