2baebd9930d29352bd701ce67f9ab6502af5d583
6heLL66 <51162582+6heLL66@users.noreply.github.com>
2021-04-23 17:19:13 +0300

    hello world

---
+++ index.js
@@ -0,0 +1,27 @@
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost'
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => {
+      return 'HELLO WORLD'
+    }
+  })
+
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
+});
+
+init()
